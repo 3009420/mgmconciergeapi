3e8f4939d6a0e7e76b3e3a13448fdb0e904c9407
Bryan Knight <bjknight1980@gmail.com>
2015-11-17 00:32:35 -0800

    adds new files

---
+++ controllers/categories.js
@@ -0,0 +1,9 @@
+/*document global*/
+"use strict";
+
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=categories.js.map
